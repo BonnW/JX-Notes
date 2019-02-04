21b015c50e40cc104e611d15512b1a380b2ee09a
bonnw <pwonghansa@gmail.com>
2019-02-04 12:20:32 -0600

    first commit java notes

---
+++ Assignments/EmployeeAPP/src/employeeApp/Company.java
@@ -0,0 +1,11 @@
+package employeeApp;
+
+public class Company;
+{
+  private static int maxId = 0;
+  public int id;l
+  public String name;
+  public int debt;
+
+  public final static double match401k = 0.05;
+}
---
+++ Assignments/EmployeeAPP/src/employeeApp/Healthplan.java
@@ -0,0 +1,23 @@
+package employeeApp
+
+public class Healthplan
+{
+  private static int maxId = 0;
+  private int id;
+  private String name;
+
+  public HealthPlan (String name)
+  {
+    this.name = name;
+  }
+
+  public int getId()
+  {
+    return id;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+}
